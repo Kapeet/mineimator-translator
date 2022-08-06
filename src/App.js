c086575ab18da641645baffb5279bc677e5127df
Kapeet <itamar246@gmail.com>
2022-08-06 22:58:24 +0300

    fixed json parsing

--- src/App.js
+++ src/App.js
@@ -12,17 +12,19 @@ function App() {
 
   const onInitiatedTranslate = () => {
     const translatedJson = parseJsonObject(languageFileJson);
-    console.log(translatedJson);
     setTranslatedJson(translatedJson);
 }
-  useEffect(() => {
-console.log(languageFileJson)
-  })
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <button disabled={!languageFileJson} onClick={onInitiatedTranslate}>deez</button>
+        <pre style={{textAlign: 'start'}}>
+          <code>
+            {JSON.stringify(translatedJson, null, 2)}
+          </code>
+        </pre>
       </header>
     </div>
   );
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,17 +4,21 @@ import isObject from "lodash/isObject";
 import translate from 'translate'
 
 
-export const parseJsonObject = async json => {
+export const parseJsonObject = json => {
     const jsonObjectIsNotValid = !Object.entries(json).length || !json
     if (jsonObjectIsNotValid) return;
 
-    const newJson = Object.entries(json).map(([key, value]) => {
-        console.log({key,value})
+    const newJson = Object.entries(json).map(([key, value], index) => {
+        if (index > 6) return;
+        //some values are actually objects, so we are using a recursion here to catch every object type value
         if (isObject(value)) {
             return parseJsonObject(value);
         }
+        // const newValue = translate(value, "es")
+        const newValue = value;
+        return {[key]: newValue};
 
     });
-
+    console.log({newJson })
     return newJson;
 }
