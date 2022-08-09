6b09a5fbc90dc7afb3450f160c777fde30fab5f4
Kapeet <itamar246@gmail.com>
2022-08-09 20:30:41 +0300

    mui styling, added icon

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
+import logo from './assets/icon.png';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import languageJson from './assets/language.json'
 import { parseJsonObject } from './utils/helpers';
 import ISO6391 from 'iso-639-1'
+import { Button, CircularProgress, MenuItem, Select } from '@mui/material';
 
 function App() {
 
   const [translatedJson, setTranslatedJson] = useState(null)
-  const [selectedLanguage, setSelectedLanguage] = useState(ISO6391.getAllNames()[0]);
+  const [selectedLanguage, setSelectedLanguage] = useState(ISO6391.getAllNames()[1]);
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState('')
   const onInitiatedTranslate = async language => {
@@ -23,27 +24,31 @@ function App() {
     setLoading(false);
 }
 
+console.log(selectedLanguage)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <label>Choose a language to translate to:</label>
-        <select value={selectedLanguage} onChange={e => setSelectedLanguage(e.target.value)}>
-          {ISO6391.getAllNames().map(language => <option key={language}>{language}</option>)}
-        </select>
-        <br />
-        <button disabled={!languageJson} onClick={() => onInitiatedTranslate(selectedLanguage)}>Translate!</button>
+        <div style={{height: 10}} />
+        <Select variant='filled' value={selectedLanguage} placeholder='Select a language...' onChange={e => setSelectedLanguage(e.target.value)}>
+        {ISO6391.getAllNames().map(language => <MenuItem key={language} value={language}>{language}</MenuItem>)}
+        </Select>
+        <div style={{height: 10}} />
+        {loading ? <CircularProgress /> :
+        <Button variant='contained' disabled={!languageJson} onClick={() => onInitiatedTranslate(selectedLanguage)}>Translate!</Button>
+      }
+      <div style={{height: 10}} />
         {error ? error : null}
-        {loading ? 'Processing, please wait...' :
-        (
+      <div style={{height: 10}} />
         <pre style={{textAlign: 'start', fontSize: 9}}>
           <code>
             {JSON.stringify(translatedJson) ?JSON.stringify(translatedJson, null, 2) : null}
           </code>
         </pre>
 
-        )
-        }
+        
+        
       </header>
     </div>
   );
